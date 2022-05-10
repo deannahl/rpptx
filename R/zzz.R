7ba77c5808808bfdd93f8623ff9835410e4763b5
Justin Kracht <justin.kracht@genmills.com>
2022-05-10 10:54:01 -0500

    Remove code to ask users if they want to install python because calling menu() non-interactively breaks the install process.

--- R/zzz.R
+++ R/zzz.R
@@ -4,21 +4,6 @@ pptx <- NULL
 # Make pptx accessible on package load
 .onLoad <- function(libname, pkgname) {
 
-  # Check if Python is installed and prompt the user to install if not
-  python_is_installed <- reticulate::py_available()
-  if (python_is_installed != TRUE) {
-    wants_to_install_python <- menu(
-      choices = c("Yes", "No"),
-      title = "No Python installation was detected. Would you like to install one now?"
-    )
-
-    if (wants_to_install_python == 1) {
-      reticulate::install_miniconda()
-    } else {
-      print("Cancelling installation")
-    }
-  }
-
   # Load the pptx module
   pptx <<- reticulate::import("pptx", delay_load = TRUE)
 
